b2d477f30a26ce79a0a9cc9f477eaedaedc52d51
Dmitri Smirnov <d.s@plexoos.com>
2014-12-08 11:01:57 -0500

    scripts: Keep only one up-to-date script for copying asym results to another host

--- script/copy_results.sh
+++ script/copy_results.sh
@@ -1,17 +1,29 @@
 #!/bin/bash
 
-LOG="/eic/u/dsmirnov/eic0005/run12/copy_results.log"
-CHECKINGPERIOD=600 # in sec
+# To run this script in background do
+#
+# >eval `ssh-agent -s`
+# >ssh-add
+#
+# Then modify the SSH_... variables in this script accordingly
+#
+# >nohup ./copy_results_pc2pc.sh run12
+#
 
-export SSH_AUTH_SOCK=/tmp/ssh-lxuXX32554/agent.32554
-export SSH_AGENT_PID=32555
+RUN_PERIOD=$1
+
+LOG="/eic/u/dsmirnov/eic0005/copy_results_pc2pc_${RUN_PERIOD}.log"
+CHECKINGPERIOD=3600 # in sec
+
+export SSH_AUTH_SOCK=/tmp/ssh-lKLHL25172/agent.25172
+export SSH_AGENT_PID=25173
 
 while [ 1 ];
 do
    echo "---" >> ${LOG}
    date >> ${LOG}
 
-   rsync --stats --bwlimit=10000 -a /eicdata/eic0005/run12/root/* bluepc:/usr/local/polarim/root/ >> ${LOG} 2>&1
+   rsync --stats --bwlimit=100000 --exclude=*.root -aO /eicdata/eic0005/${RUN_PERIOD}/root/* pc2pc-phy:/usr/local/polarim/root/ >> ${LOG} 2>&1
 
    sleep $CHECKINGPERIOD
 done
